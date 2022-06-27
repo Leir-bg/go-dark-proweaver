311ff22f6567dddc987f44beadf415df9fbfaca3
Leir-bg <108060832+Leir-bg@users.noreply.github.com>
2022-06-27 15:38:42 +0800

    Go Dark version 1
    Go Dark version 1

---
+++ go-dark/go-dark.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Go Dark Proweaver
+ * Plugin URI: https://web4.proweaverlinks.com/MAINTENANCE/FORRESEARCH/
+ * Description: Dark mode for Proweaver themes.
+ * Version: 1.0
+ * Author: PRD1113, PRD978
+ * Author URI: https://web4.proweaverlinks.com/MAINTENANCE/FORRESEARCH/
+ */
+add_action('admin_menu', 'test_plugin_setup_menu');
+
+function test_plugin_setup_menu(){
+    add_menu_page( 'Go Dark', 'Go Dark', 'manage_options', 'go-dark', 'test_init');
+}
+
+function test_init(){
+    echo "<h1>Hello World!</h1>";
+}
+
+/** Hook Dark Mode */
+
+add_action('wp_enqueue_scripts', 'load_dm');
+
+function load_dm(){
+    wp_register_style('darkmode_style', plugin_dir_url( __FILE__ ) . 'assets/css/darkstyle.min.css');
+    wp_enqueue_style('darkmode_style');
+    wp_enqueue_script('darkmode_script', plugin_dir_url( __FILE__ ) . 'scripts/darkmode.min.js', array('jquery'));
+}
+?>
---
+++ go-dark/scripts/darkmode.js
@@ -0,0 +1,209 @@
+/** TRIGGER DARK MODE */
+jQuery(document).ready(function(){
+	checkStatus(localStorage.getItem('status'))
+ 
+	var btn = document.createElement('div')
+	btn.className = 'btnDM'
+	btn.appendChild(document.createElement('input'))
+	btn.appendChild(document.createElement('span'))
+	$('footer')[0].append(btn)
+
+	$('.btnDM').click(function(){
+		if(localStorage.getItem('status') != 1){
+			localStorage.setItem('status', 1)
+			setDark();
+		}else{
+			localStorage.setItem('status', 0)
+			setDefault()
+		}
+		$('.btnDM').toggleClass('active');
+		//console.log(localStorage.getItem('status'))
+	})
+	
+	checkBtnStatus(localStorage.getItem('status'))
+})
+
+/** DARK MODE FUNCTIONS */
+const defaultExcluded = 'html *:not(script, style, noscript, figure, img, #banner, #banner *:not(.bnr_info, .bnr_info *), .wrapper, .btnDM, .toggle_holder, .toggle_holder *, .toggle_nav_close, .toggle_right_nav.toggle_right_cont, .toggle_right_nav, .toggle_right_cont'
+
+//mga elementes nga dili nimo ipaapil ug dark mode
+var exemptElem = '#popupVSButton *, #popupVSChat *, .serv_list ul li a, .modal, .main_btn, .bnr_info a, .testimonial_holder a, .btm2_left ul li, .btm2_left ul li *'
+
+//"call to" functions
+/** SET TO DARK MODE */
+function setDark(){
+	// $(defaultExcluded+','+exemptElem+')').each(function(){
+	// 	$(this).css('background', '#222')
+	// 	$(this).css('color', '#fff')
+	// })
+    $('.main_con, .btm1_con, .footer_top_con, .footer_nav').addClass('dm');
+
+    $('header, #nav_area, #main_area, #bottom2, .footer_btm').css({'background':'#222','color':'#fff'});
+    $('header, #nav_area, #main_area, #bottom2, .footer_btm').find('*').css({'background':'#222','color':'#fff'});
+
+    $('.bnr_info, #bottom1, .footer_top').css({'background':'#333','color':'#fff'});
+    $('.bnr_info, #bottom1, .footer_top').find('*').css({'background':'#333','color':'#fff'});
+
+    $('header, #main_area, #bottom2, .bnr_info, #bottom1').find('a, a span').css({'background':'','color':''});
+}
+
+/** REVERT TO DEFAULT */
+function setDefault(){
+	// $('html *:not('+exemptElem+')').each(function(){
+	// 	$(this).css('background', '')
+	// 	$(this).css('color', '')
+	// })
+    $('.main_con, .btm1_con, .footer_top_con, .footer_nav').removeClass('dm');
+
+    $('header, #nav_area, #main_area, #bottom2').css({'background':'','color':''});
+    $('header, #nav_area, #main_area, #bottom2').find('*').css({'background':'','color':''});
+
+    $('.bnr_info, #bottom1, footer').css({'background':'','color':''});
+    $('.bnr_info, #bottom1, footer').find('*').css({'background':'','color':''});
+}
+
+//mucheck if naka darkmode ang site
+function checkStatus(status){
+	if(status != 1){
+		return
+	}else{
+		setDark();
+	}
+}
+
+function checkBtnStatus(status){ //temporary
+	if(status != 1){
+		return
+	}else{
+		$('.btnDM').addClass('active');
+	}
+}
+
+
+/** ------------------------------------------------------------------------------------------ */
+/** ------- BASURA NANI MGA GI COMMENT SA UBOS BUT WALA LANG GI DELETE FOR REFERENCE -------*/
+
+
+
+
+// var elemsWPseudoClass = ['.main_con', '.btm1_con', '.footer_top_con'] //temporary
+
+// elemsWPseudoClass.forEach((elem) => {
+//   //console.log(elem);
+//   var el = document.querySelector(elem)
+//   var after = window.getComputedStyle(el, '::after');
+//   var before = window.getComputedStyle(el, '::before');
+
+//   if(after['content'] != 'none' || before['content'] != 'none'){
+// 	  $(elem+'::after').css('display', 'none');
+// 	  console.log(elem+'::after')
+	  
+// 	  $(elem+'::before').css('display', 'none');
+// 	  console.log(elem+'::before')
+//   }
+//   if(before['content'] != 'none'){
+// 	  $(elem+'::before').css('display', 'none');
+// 	  console.log(elem+'::before')
+//   }
+//   if(after['content'] != 'none'){
+// 	  $(elem+'::after').css('display', 'none');
+// 	  console.log(elem+'::after')
+//   }
+  
+// })
+
+// /**DARK MODE START */
+// $(document).ready(function(){
+// 	var dm = document.createElement('a')
+// 	dm.className = 'btnDM'
+// 	$('footer')[0].append(dm)
+
+// 	$('.btnDM').click(function(){
+// 		if(localStorage.getItem('status') != 1){
+// 			darkModeSwitch(localStorage.setItem('status', 1));
+// 		}else{
+// 			darkModeSwitch(localStorage.setItem('status', 0));
+// 		}
+// 		location.reload();
+// 		console.log(localStorage.getItem('status'))
+// 	})
+// // })
+
+
+// /** APPLY DARK MODE - START */
+// //localStorage.setItem('status', 0) //1 = darkmode-on
+// function darkModeSwitch(status){
+// 	if(status != 1){
+// 		return
+// 	}else{
+// 		localStorage.setItem('status', 1)
+// 		var link = document.createElement('link')
+// 		link.rel = 'stylesheet'
+// 		link.type = 'text/css'
+// 		link.href = 'wp-content/themes/bestvaluehealthcare/darkstyle.css'
+// 		link.media = 'all'
+// 		link.id = 'darkStyle'
+
+// 		var head = document.getElementsByTagName('head')[0];
+// 		head.append(link);
+// 	}
+// 	console.log(localStorage.getItem('status'))
+// }darkModeSwitch(localStorage.getItem('status'))
+// /** APPLY DARK MODE - END*/
+
+
+
+// /** DARK MODE FUNCTION - TEST ONLY */
+
+// function getAllColors() {
+// 	// regex via http://stackoverflow.com/a/7543829/149636
+// 	var rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
+
+// 	var allColors = [];
+
+// 	var elems = document.getElementsByTagName('*');
+// 	var total = elems.length;
+
+// 	var x,y,elemStyles,styleName,styleValue,rgbVal;
+
+// 	for(x = 0; x < total; x++) {
+// 		elemStyles = window.getComputedStyle(elems[x]);
+
+// 		for(y = 0; y < elemStyles.length; y++) {
+// 			styleName = elemStyles[y];
+// 			styleValue = elemStyles[styleName];
+// 			if(!styleValue) {
+// 				continue;
+// 			}
+
+// 			// convert to string to avoid match exceptions
+// 			styleValue += "";
+			
+// 			rgbVal = styleValue.match(rgbRegex);
+// 			if(!rgbVal) { // property does not contain a color
+// 				continue;
+// 			}
+
+// 			if(allColors.indexOf(rgbVal.input) == -1) { // avoid duplicate entries
+// 				allColors.push(rgbVal.input);
+// 				//console.log(rgbVal)
+// 			}
+// 		}
+// 	}return allColors;
+// }
+// //console.log(getAllColors().join('\n'))
+// //console.log(getAllColors())
+
+// function getAllElements(){
+	
+// }
+
+// function matchElementsToColors(colorArray){
+// 	var colorVal = colorArray;
+// 	for(var i = 0; i < colorVal.length; i++){
+// 		console.log(colorVal[i])
+// 	}
+// }matchElementsToColors(getAllColors())
+
+// /** DARK MODE FUNCTION - END HERE */
+// /**DARK MODE END */
